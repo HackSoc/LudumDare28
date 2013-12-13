2b1aa9c14fad21810136b8154899d4d90cd3f307
Frank Burton <frankrburton@nas31-13.york.ac.uk>
2013-12-13 23:31:14 +0000

    And assets

---
+++ main.lua
@@ -0,0 +1,32 @@
+
+local scene = require 'scene'
+
+function love.load()
+	scene.initialize()
+	
+end
+
+
+function love.update(dt)
+	scene.update(dt)
+	
+end
+
+
+function love.keypressed(key, unicode)
+	
+	
+end
+
+
+function love.keyreleased(key, unicode)
+	
+end
+
+
+function love.draw()
+	scene.draw()
+	
+end	
+
+

6bdbb479dd8adb42104db5c75701628e50d94e07
Michael Walker <mike@barrucadu.co.uk>
2013-12-14 23:54:11 +0000

    Enemies can be killed, mercilessly

--- entity/Enemy.lua
+++ entity/Enemy.lua
@@ -4,6 +4,7 @@ require 'entity.Mob'
 Enemy = class('entity.Enemy', Mob)
 Enemy.static.sprite = love.graphics.newImage("assets/character-3.png")
 Enemy.static.speed = 5
+Enemy.health = 5
 
 function Enemy:initialize(id, x, y, collider)
    Entity.initialize(self, id, x, y, self.class.sprite, collider)

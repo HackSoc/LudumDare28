6810f3f10218d6e38e0d3bbbb3226c74c10fd7db
Top Cat <webmaster@thomasc.co.uk>
2013-12-14 23:49:23 +0000

    Don't collide tiles with bullets

--- entity/Tile.lua
+++ entity/Tile.lua
@@ -13,7 +13,7 @@ function Tile:initialize(id, x, y, sprite, collider, nocollision)
 end
 
 function Tile:hit(other, dx, dy)
-    if other.class ~= Tile then
+    if other:isInstanceOf(Mob) then
         other:move(other.x, self.y - self.height/2 - other.height/2)
     end
 end

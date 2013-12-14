c66af8f44730a3407ef4815b9f9321242aab738d
Frank Burton <frankrburton@nas31-13.york.ac.uk>
2013-12-14 04:39:15 +0000

    Fixed

--- main.lua
+++ main.lua
@@ -1,12 +1,9 @@
 require 'entity.Entity'
-<<<<<<< HEAD
 require 'entity.Ball'
 require 'entity.Paddle'
 require 'entity.OneBullet'
 
-=======
 require 'entity.Player'
->>>>>>> f0c799003b54a0ac5e692487557ee0a3953c4360
 local scene = require 'scene'
 
 function love.load()
@@ -16,13 +13,9 @@ function love.load()
 
     player = Player:new(90, 250, scene.collider)
 
-<<<<<<< HEAD
     wallTop = Wall:new(400, -25, 800, 50, scene.collider)
     wallBottom = Wall:new(400, 625, 800, 50, scene.collider)
 
-    scene.addEntity(paddleA)
-    scene.addEntity(paddleB)
-    scene.addEntity(ball)
     scene.addEntity(wallTop)
     scene.addEntity(wallBottom)
     
@@ -30,9 +23,7 @@ function love.load()
     bullet = OneBullet:new(scene.collider)
     scene.addEntity(bullet)
     
-=======
     scene.addEntity(player)
->>>>>>> f0c799003b54a0ac5e692487557ee0a3953c4360
 end
 
 
@@ -55,20 +46,16 @@ function love.keypressed(key, unicode)
     if key == 'up' then
         keyUp = true
     elseif key == 'down' then
-<<<<<<< HEAD
-        padDown = true
+        keyDown = true
     elseif key == 'k' then
         bullet:goLeft()
     elseif key == 'l' then
         bullet:goRight()
-=======
-        keyDown = true
     end
     if key == 'left' then
         keyLeft = true
     elseif key == 'right' then
         keyRight = true
->>>>>>> f0c799003b54a0ac5e692487557ee0a3953c4360
     end
     
 end
@@ -77,20 +64,15 @@ function love.keyreleased(key, unicode)
     if key == 'up' then
         keyUp = false
     elseif key == 'down' then
-<<<<<<< HEAD
         padDown = false
     elseif key == 'k' then
         bullet:stopLeft()
     elseif key == 'l' then
         bullet:stopRight()
-=======
-        keyDown = false
-    end
-    if key == 'left' then
+    elseif key == 'left' then
         keyLeft = false
     elseif key == 'right' then
         keyRight = false
->>>>>>> f0c799003b54a0ac5e692487557ee0a3953c4360
     end
 end
 

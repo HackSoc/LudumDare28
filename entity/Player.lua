b42dcd367437329c93f9a4e6795f4983012a0c61
Michael Walker <mike@barrucadu.co.uk>
2013-12-14 19:09:21 +0000

    Player fades.

--- entity/Player.lua
+++ entity/Player.lua
@@ -1,12 +1,17 @@
 local class = require 'middleclass.middleclass'
 require 'entity.Entity'
 
+local highestiteration = 0
+
 Player = class('entity.Player', Entity)
 Player.static.sprite = love.graphics.newImage("assets/block.png")
 Player.static.speed = 10
+Player.iteration = 0
 
 function Player:initialize(x, y, collider)
     Entity.initialize(self, x, y, self.class.sprite, collider)
+    highestiteration = highestiteration + 1
+    self.iteration = highestiteration
 end
 
 function Player:startLeft()
@@ -15,4 +20,11 @@ end
 
 function Player:startRight()
    self.dx = self.class.speed
+end
+
+function Player:draw()
+    local opacity = (5 - highestiteration + self.iteration) / 5
+    love.graphics.setColor(255,255,255, 255 * opacity)
+    Entity.draw(self)
+    love.graphics.setColor(255,255,255,255)
 end

2bd122ce60f0b74aa479d6cd26166fb0bd1b34d4
Michael Walker <mike@barrucadu.co.uk>
2013-12-14 22:47:54 +0000

    Remove needless constructor from Tile

--- entity/Tile.lua
+++ entity/Tile.lua
@@ -3,10 +3,6 @@ require 'entity.Entity'
 
 Tile = class('entity.Tile', Entity)
 
-function Tile:initialize(id, x, y, sprite, collider)
-    Entity.initialize(self, id, x, y, sprite, collider)
-end
-
 function Tile:hit(other, dx, dy)
     if other.class ~= Tile then
         other:move(other.x, self.y - self.height/2 - other.height/2)

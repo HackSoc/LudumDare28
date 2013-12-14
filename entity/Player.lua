f406b6721d742bddca1ab2ad80663513d3f1057a
James Baxter <jebaxter@hotmail.co.uk>
2013-12-14 17:15:13 +0000

    Add Player

---
+++ entity/Player.lua
@@ -0,0 +1,18 @@
+local class = require 'middleclass.middleclass'
+require 'entity.Entity'
+
+Player = class('entity.Player', Entity)
+Player.static.sprite = love.graphics.newImage("assets/block.png")
+Player.static.speed = 1
+
+function Player:initialize(x, y, collider)
+    Entity.initialize(self, x, y, self.class.sprite, collider)
+end
+
+function Player:startLeft()
+   self.dx = -self.class.speed
+end
+
+function Player:startRight()
+   self.dx = self.class.speed
+end

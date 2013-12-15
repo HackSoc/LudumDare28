197c420293f45a78c1c8ea1dbf3d127ff4a8dcd6
Michael Walker <mike@barrucadu.co.uk>
2013-12-15 16:13:10 +0000

    Remove empty Tile:tick()

--- entity/Tile.lua
+++ entity/Tile.lua
@@ -8,9 +8,6 @@ function Tile:initialize(id, x, y, sprite, collider, nocollision)
     Entity.initialize(self, id, x, y, sprite, collider)
 end
 
-function Tile:tick(state, collider)
-end
-
 function Tile:hit(other, dx, dy)
     if other:isInstanceOf(Mob) then
         other:move(other.x - dx, other.y - dy)
